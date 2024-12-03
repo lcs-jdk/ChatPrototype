3d9b1c7f7a93a4bd0d11094160ba0b90f44ac220
Jayden Kim <hkim@lcs.on.ca>
2024-12-03 08:47:01 -0500

    Initial Commit

---
+++ ChatPrototype/ChatPrototypeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatPrototypeApp.swift
+//  ChatPrototype
+//
+//  Created by kim heejun on 12/3/24.
+//
+
+import SwiftUI
+
+@main
+struct ChatPrototypeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
